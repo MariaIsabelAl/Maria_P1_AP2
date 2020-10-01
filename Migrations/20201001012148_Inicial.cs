9e3af736ff6c4617c70f0ae643d6cf6957e75ec1
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-09-30 21:31:23 -0400

    Registro y Consulta

---
+++ Migrations/20201001012148_Inicial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Maria_P1_AP2.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.CategoriaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
--- Models/Categorias.cs
+++ Models/Categorias.cs
@@ -9,6 +9,7 @@ namespace Maria_P1_AP2.Models
     public class Categorias
     {
         [Key]
+         [Range(0,100000, ErrorMessage ="El rango es de 0 a 100000")]
         public int CategoriaId { get; set; }
 
         [Required(ErrorMessage ="La descripción es obligatoria")]
